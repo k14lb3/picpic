c91228eda3943dba8974a82164b9509f7c44328e
k14lb3 <k14lb3@gmail.com>
2022-03-11 00:05:05 +0800

    Add Button component

---
+++ src/components/Button.js
@@ -0,0 +1,20 @@
+const VARIANT = {
+  DEFAULT:
+    'bg-downy border-downy text-white hover:bg-downy-600 hover:border-downy-600',
+  OUTLINED: 'border-downy text-downy hover:bg-downy-50',
+};
+
+const Button = ({ outlined, className, children, ...props }) => {
+  return (
+    <button
+      className={`px-2 py-1 border-solid border rounded-sm text-sm font-medium tracking-wide ease-in duration-200 ${
+        outlined ? VARIANT.OUTLINED : VARIANT.DEFAULT
+      }${className ? ` ${className}` : ''}`}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
