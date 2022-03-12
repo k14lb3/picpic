d33b5983f8aa425e749856c6bd3e4a72b3f1de85
k14lb3 <k14lb3@gmail.com>
2022-03-12 19:55:52 +0800

    Add forwardRef

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -1,6 +1,9 @@
-const InputText = ({ className, ...props }) => {
+import { forwardRef } from 'react';
+
+const InputText = forwardRef(({ className, ...props }, ref) => {
   return (
     <input
+      ref={ref}
       className={`w-full px-2 py-1 bg-downy-100 border-solid border border-downy rounded-sm outline-none${
         className ? ` ${className}` : ''
       }`}
@@ -8,6 +11,6 @@ const InputText = ({ className, ...props }) => {
       {...props}
     />
   );
-};
+});
 
 export default InputText;
