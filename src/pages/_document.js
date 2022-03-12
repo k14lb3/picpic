61f64ea8541739e61746a1ec62bd5432bca02d2d
k14lb3 <k14lb3@gmail.com>
2022-03-12 18:30:50 +0800

    CORS error bug still persists, put crossOrigin property in nextConfig instead 'https://github.com/vercel/next.js/blob/main/errors/doc-crossorigin-deprecated.md'

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,11 +5,7 @@ const Document = () => {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin='anonymous'
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
