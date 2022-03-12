49e6d72bb3a427a942afb12da4842f2aacdda0c2
k14lb3 <k14lb3@gmail.com>
2022-03-12 20:51:51 +0800

    Change padding

--- src/components/Button.js
+++ src/components/Button.js
@@ -7,7 +7,7 @@ const VARIANT = {
 const Button = ({ outlined, className, children, ...props }) => {
   return (
     <button
-      className={`px-3 py-1.5 border-solid border rounded-sm font-medium tracking-wide ease-in duration-200 outline-none select-none ${
+      className={`px-3 py-1 border-solid border rounded-sm font-medium tracking-wide ease-in duration-200 outline-none select-none ${
         outlined ? VARIANT.OUTLINED : VARIANT.DEFAULT
       }${className ? ` ${className}` : ''}`}
       {...props}
