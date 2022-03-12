65582cdc45fb79e1e921c4d353e441566b6b6923
k14lb3 <k14lb3@gmail.com>
2022-03-12 19:56:46 +0800

    Add type props, and make it 'text' as default

--- src/components/InputText.js
+++ src/components/InputText.js
@@ -1,13 +1,13 @@
 import { forwardRef } from 'react';
 
-const InputText = forwardRef(({ className, ...props }, ref) => {
+const InputText = forwardRef(({ className, type = 'text', ...props }, ref) => {
   return (
     <input
       ref={ref}
       className={`w-full px-2 py-1 bg-downy-100 border-solid border border-downy rounded-sm outline-none${
         className ? ` ${className}` : ''
       }`}
-      type="text"
+      type={type}
       {...props}
     />
   );
