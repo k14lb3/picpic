8a5a5e13d17296c1150ce523a270bee4db2fdb7a
k14lb3 <k14lb3@gmail.com>
2022-03-14 19:56:15 +0800

    Add Loader component

---
+++ src/components/Loader.js
@@ -0,0 +1,32 @@
+const SIZE = {
+  SM: 'after:w-6 after:h-6 after:border-4',
+  MD: 'after:w-9 after:h-9 after:border-[6px]',
+  LG: 'after:w-12 after:h-12 after:border-8',
+};
+
+const Loader = ({ size, color, bgColor, className, ...props }) => {
+  return (
+    <div
+      className={`flex justify-center after:content-[''] ${
+        size === 'sm'
+          ? SIZE.SM
+          : size === 'md'
+          ? SIZE.MD
+          : size === 'lg'
+          ? SIZE.LG
+          : ''
+      } after:border-solid after:border-${
+        bgColor ? bgColor : 'downy-100'
+      } after:border-t-${
+        color ? color : 'downy'
+      } after:rounded-full animate-spin${className ? ` ${className}` : ''}`}
+      {...props}
+    ></div>
+  );
+};
+
+Loader.defaultProps = {
+  size: 'sm',
+};
+
+export default Loader;
