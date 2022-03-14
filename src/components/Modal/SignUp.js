34eb1ffe77299fae5336a1ae8a324bcd6944501c
k14lb3 <k14lb3@gmail.com>
2022-03-14 23:21:35 +0800

    Add functionality for the Sign Up function

--- src/components/Modal/SignUp.js
+++ src/components/Modal/SignUp.js
@@ -6,8 +6,11 @@ import { modalState } from 'recoil/atoms';
 import Button from 'components/Button';
 import InputText from 'components/InputText';
 import { MODAL } from 'components/Modal';
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import Loader from 'components/Loader';
+import {
+  createUserWithEmailAndPassword,
+  sendEmailVerification,
+  updateProfile,
+} from 'firebase/auth';
 
 // https://www.simplilearn.com/tutorials/javascript-tutorial/email-validation-in-javascript
 
@@ -17,12 +20,13 @@ const isValidEmail = (email) =>
 const isValidUsername = (username) => /^[a-zA-Z0-9\_\.]+$/.test(username);
 
 const SignUp = () => {
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
   const emailRef = useRef(null);
   const usernameRef = useRef(null);
   const passwordRef = useRef(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
   const [signUpDisabled, setSignUpDisabled] = useState(true);
+  const [verificationSent, setVerificationSent] = useState(false);
   const setModal = useSetRecoilState(modalState);
 
   const signUp = async (e) => {
@@ -55,19 +59,29 @@ const SignUp = () => {
       return;
     }
 
-    // try {
-    //   const userCred = await createUserWithEmailAndPassword(
-    //     auth,
-    //     email,
-    //     password
-    //   );
-    //   await userCred.user.sendEmailVerification({
-    //     url: window.location.href,
-    //   });
-    //   console.log(userCred);
-    // } catch (e) {
-    //   console.log(e.message);
-    // }
+    try {
+      setLoading(true);
+      const userCred = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      await updateProfile(auth.currentUser, {
+        displayName: username,
+        photoURL:
+          'https://firebasestorage.googleapis.com/v0/b/picpic-59a20.appspot.com/o/stock-imgs%2Fdp.png?alt=media&token=64fa4f1b-189c-4c48-a881-a7a65981d0fb',
+      });
+
+      await sendEmailVerification(userCred.user);
+
+      setVerificationSent(true);
+    } catch (e) {
+      if (e.message === 'Firebase: Error (auth/email-already-in-use).') {
+        setError('Email already in use.');
+      }
+    }
+    setLoading(false);
   };
 
   const inputOnChange = () => {
@@ -93,49 +107,78 @@ const SignUp = () => {
         />
       </div>
       <div className="px-4">
-        <form onSubmit={signUp} noValidate>
-          <InputText
-            ref={emailRef}
-            className="mb-2"
-            type="email"
-            placeholder="Email"
-            onChange={inputOnChange}
-          />
-          <InputText
-            ref={usernameRef}
-            className="mb-2"
-            placeholder="Username"
-            onChange={inputOnChange}
-          />
-          <InputText
-            ref={passwordRef}
-            type="password"
-            placeholder="Password"
-            hasShowPassword
-            onChange={inputOnChange}
-          />
-          <Button
-            disabled={signUpDisabled}
-            loading={loading}
-            className="w-full mt-4 mb-2"
-            label="Sign Up"
-          />
-        </form>
-        <div className="flex">
-          <p>
-            Already have an account?
-            <span
-              className="text-downy ml-1 cursor-pointer hover:underline"
-              onClick={() => setModal(MODAL.LOGIN)}
-            >
-              Log In
-            </span>
-          </p>
-        </div>
-        {error && (
-          <div className="mt-4 text-center">
-            <p className="text-red-500">{error}</p>
-          </div>
+        {verificationSent ? (
+          <>
+            <h2 className="font-bold text-xl text-left mb-4">
+              Hello {auth.currentUser.displayName},
+            </h2>
+            <p className="mb-8 text-center">
+              Thanks for signing up with Picpic! We just need to verify if it's
+              really yours. We've sent an email to{' '}
+              <span className="font-bold">{auth.currentUser.email}</span>.
+            </p>
+            <Button
+              className="block mx-auto"
+              loading={loading}
+              label="Resend email"
+              onClick={async () => {
+                setLoading(true);
+
+                try {
+                  await sendEmailVerification(auth.currentUser);
+                } catch (e) {}
+
+                setLoading(false);
+              }}
+            />
+          </>
+        ) : (
+          <>
+            <form onSubmit={signUp} noValidate>
+              <InputText
+                ref={emailRef}
+                className="mb-2"
+                type="email"
+                placeholder="Email"
+                onChange={inputOnChange}
+              />
+              <InputText
+                ref={usernameRef}
+                className="mb-2"
+                placeholder="Username"
+                onChange={inputOnChange}
+              />
+              <InputText
+                ref={passwordRef}
+                type="password"
+                placeholder="Password"
+                hasShowPassword
+                onChange={inputOnChange}
+              />
+              <Button
+                className="w-full mt-4 mb-2"
+                disabled={signUpDisabled}
+                loading={loading}
+                label="Sign Up"
+              />
+            </form>
+            <div className="flex">
+              <p>
+                Already have an account?
+                <span
+                  className="text-downy ml-1 cursor-pointer hover:underline"
+                  onClick={() => setModal(MODAL.LOGIN)}
+                >
+                  Log In
+                </span>
+              </p>
+            </div>
+            {error && (
+              <div className="mt-4 text-center">
+                <p className="text-red-500">{error}</p>
+              </div>
+            )}
+          </>
         )}
       </div>
     </>
