b34ee3b8c7f025bde7e4eef4a471194494e194db
k14lb3 <k14lb3@gmail.com>
2022-03-15 19:31:38 +0800

    Add server timestamp document reference

--- src/firebase/refs.js
+++ src/firebase/refs.js
@@ -1,6 +1,8 @@
 import { db } from './config';
 import { collection, doc } from 'firebase/firestore';
 
+export const serverCol = collection(db, 'server');
+export const serverTimestampDoc = doc(serverCol, 'timestamp');
 export const usersCol = collection(db, 'users');
 export const userDoc = (id) => doc(usersCol, id);
 export const usersUnverifiedCol = collection(db, 'users_unverified');
