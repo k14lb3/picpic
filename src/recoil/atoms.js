fbcfeb9c7136878f5cf42e1abc60209591de6e4b
k14lb3 <k14lb3@gmail.com>
2022-03-15 15:40:31 +0800

    Add currentUserState that will contain the currently logged in user

--- src/recoil/atoms.js
+++ src/recoil/atoms.js
@@ -1,5 +1,10 @@
 import { atom } from 'recoil';
 
+export const currentUserState = atom({
+  key: 'currentUserState',
+  default: null,
+});
+
 export const modalState = atom({
   key: 'modalState',
   default: null,
