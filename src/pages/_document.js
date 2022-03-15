b7c93e0e863cd9d3a80de2c238cebcb88abec6f8
k14lb3 <k14lb3@gmail.com>
2022-03-15 19:59:18 +0800

    Fix CORS ISSUE AGAIN -_-

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,7 @@ const Document = () => {
         <link
           href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap"
           rel="stylesheet"
+          crossOrigin='anonymous'
         />
       </Head>
       <body>
