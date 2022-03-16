b73883813c7649b36702e59439f81c823b841622
k14lb3 <k14lb3@gmail.com>
2022-03-16 23:43:50 +0800

    Prevent the icons from shrinking

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,7 +10,7 @@ const SearchBar = () => {
     <div className="flex items-center w-full max-w-xs h-full max-h-9 bg-downy-100 px-2 mx-10 rounded-sm">
       {!focus && (
         <SearchIcon
-          className="h-5 w-5 mr-2 text-downy cursor-text"
+          className="flex-shrink-0 h-5 w-5 mr-2 text-downy cursor-text"
           onClick={() => inputRef.current.focus()}
         />
       )}
@@ -24,7 +24,7 @@ const SearchBar = () => {
       />
       {focus && (
         <XCircleIcon
-          className="h-5 w-5 ml-2 text-downy cursor-pointer"
+          className="flex-shrink-0 h-5 w-5 ml-2 text-downy cursor-pointer"
           onMouseDown={() => {
             inputRef.current.value = '';
           }}
