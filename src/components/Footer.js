466b7e954dae667294e6fdc5b35ad8a7fc7266ad
k14lb3 <k14lb3@gmail.com>
2022-03-22 00:02:46 +0800

    Add anchor tag that has an href of my github acc lol

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,17 @@
 const Footer = () => {
   return (
     <footer className="w-full text-center pt-7 pb-14 mt-auto">
-      <span className="text-downy-300">&copy; 2022 Picpic from k14lb3</span>
+      <span className="text-downy-300">
+        &copy; 2022 Picpic from{' '}
+        <a
+          className="hover:underline"
+          target="_blank"
+          href="https://github.com/k14lb3"
+          rel="noopener noreferrer"
+        >
+          k14lb3
+        </a>
+      </span>
     </footer>
   );
 };
