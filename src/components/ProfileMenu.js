ab5e692e545919f817005622d67c90dd4809bae8
k14lb3 <k14lb3@gmail.com>
2022-03-25 18:09:13 +0800

    Change position

--- src/components/ProfileMenu.js
+++ src/components/ProfileMenu.js
@@ -15,7 +15,7 @@ const ProfileMenu = ({ close }) => {
   return (
     <>
       <div className="fixed top-0 left-0 w-screen h-screen" onClick={close} />
-      <div className="absolute top-[-157%] left-[150%] sm:top-[150%] sm:left-[-258%] w-40 bg-white rounded-sm shadow-sm shadow-gray-300">
+      <div className="absolute top-[-157%] left-[130%] sm:top-[150%] sm:left-[-258%] w-40 bg-white rounded-sm shadow-sm shadow-gray-300">
         <Link href={`/${currentUserAtom.username}`}>
           <a className="group relative profile-menu-btn rounded-t-sm">
             <UserCircleIcon />
